5746648a86a6f41dbb5868e8c79d7466c62b41e4
DouBeeTwT <493487050@qq.com>
2023-12-22 16:02:21 +0800

    Update

---
+++ Model/Neck/BiFPN.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+
+# https://github.com/zylo117/Yet-Another-EfficientDet-Pytorch/blob/master/efficientdet/model.py
+
+class BiFPN(nn.Module):
+    def __init__(self) -> None:
+        super(BiFPN, self).__init__()
+        self.
+
+    def forward(self, x):
+        pass
